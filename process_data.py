0340f9e19e1a10f03a3035031b9e63c80895592a
William <william.kang@ubc.ca>
2023-06-02 09:54:19 -0700

    Updated code

--- process_data.py
+++ process_data.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import numpy as np
 import os
+from datetime import datetime
+
+def add_date_to_filename(filename):
+    date_str = datetime.now().strftime('%Y%m%d_%H%M%S')
+    # Split the filename into name and extension
+    name, extension = os.path.splitext(filename)
+
+    # Return the filename with date appended
+    return f"{name}_{date_str}{extension}"
+
 
 def process_file(csv_path, output_file_name):
     df = pd.read_csv(csv_path)
@@ -12,18 +22,20 @@ def process_file(csv_path, output_file_name):
     df = df.drop(1)
 
     # Convert to number
-    df.iloc[:, 10] = pd.to_numeric(df.iloc[:, 10], errors='coerce')
     df.iloc[:, 9] = pd.to_numeric(df.iloc[:, 9], errors='coerce')
+    df.iloc[:, 10] = pd.to_numeric(df.iloc[:, 10], errors='coerce')
     
-    # Current grant balance is 3500 - amount they claimed
-    df.insert(loc=11, column='Current Grant balance', value=3500 - df.iloc[:, 10])
     # Amount of grant to give them is the minimum of their current grant balance with the cost of their courses
-    df.insert(loc=12, column='Grant amount to give', value=np.minimum(df['Current Grant balance'], df.iloc[:, 9]))
-
+    df.insert(loc=11, column='Grant amount to give', value=np.minimum(df.iloc[:, 10], df.iloc[:, 9]))
+    
+    df.reset_index(drop=True, inplace=True)  # Reset the index
+    
     if os.path.exists(output_file_name):
         df_existing = pd.read_excel(output_file_name)
+        
 
         df_final = pd.concat([df_existing, df])
+        
 
         df_final.drop_duplicates(subset='ResponseID', keep='first', inplace=True)
 
@@ -32,6 +44,10 @@ def process_file(csv_path, output_file_name):
 
     else:
         df_final = df
+        
         df_final.insert(loc=13, column='Processed', value='')
 
     df_final.to_excel(output_file_name, index=False)
+    date_file_name = add_date_to_filename(output_file_name)
+    print(f"ALSO SAVING TO {date_file_name} PLEASE MAKE SURE THE DATA IS SYNCED CORRECTLY")
+    df_final.to_excel(date_file_name, index=False)
